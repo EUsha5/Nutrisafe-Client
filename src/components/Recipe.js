33c07001d3ddf9aed4ceccb18f7f02d78024398c
Eran Usha <eusha5@gmail.com>
2018-10-12 15:24:05 -0400

    most recent commit

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -34,19 +34,30 @@ getRecipe = () => {
   render() {
     return(
       <div className="recipe-height">
-      <div>
-        {/* <h1>{this.state.recipe.label}</h1> */}
-      </div>
         <div className="recipe-img">
-          {/* <h1>{this.state.recipe.image}</h1> */}
+        {this.state.recipe &&
+         <img src={this.state.recipe.image} alt=""/>
+      }
         </div>
+      <div>
+      {this.state.recipe && 
+        <h1>{this.state.recipe.label}</h1>
+      }
+      </div>
         <div className="nutri-values">
           <p>Nutrional Values:</p> 
-            {/* <span>{this.state.recipe.totalNutrients.ENERC_KCAL}kcal calories</span>
-            <span>{this.state.recipe.totalNutrients.CHOCDF}g carbs</span>
-            <span>{this.state.recipe.totalNutrients.FIBTG}g fiber</span>
-            <span>{this.state.recipe.totalNutrients.PROCNT}g protien</span>
-            <span>{this.state.recipe.totalNutrients.SUGAR}g sugar</span> */}
+
+
+          {this.state.recipe && 
+            <span>{this.state.recipe.totalNutrients.ENERC_KCAL.quantity.toFixed(1)}kcal calories</span>
+          }<br/>{this.state.recipe && 
+            <span>{this.state.recipe.totalNutrients.CHOCDF.quantity.toFixed(1)}g carbs</span>}
+            <br/>{this.state.recipe && 
+            <span>{this.state.recipe.totalNutrients.FIBTG.quantity.toFixed(1)}g fiber</span>}
+            <br/>{this.state.recipe && 
+            <span>{this.state.recipe.totalNutrients.PROCNT.quantity.toFixed(1)}g protien</span>}
+            <br/>{this.state.recipe && 
+            <span>{this.state.recipe.totalNutrients.SUGAR.quantity.toFixed(1)}g sugar</span>}
           <p>Dietary Values:</p>
 
         </div>
@@ -54,13 +65,18 @@ getRecipe = () => {
           <h2 className="recipe-ingredients">Ingredients:</h2>
         </div>
         <div>
-          {/* <h3>CookTime: {this.state.recipe.totalTime}min</h3>
-          <h3>Yield: {this.state.recipe.yield}</h3> */}
+        {this.state.recipe && 
+          <h3>CookTime: {this.state.recipe.totalTime}min</h3>}
+          {this.state.recipe && 
+          <h3>Yield: {this.state.recipe.yield}</h3>
+        }
         </div>
         <div>
           <h2>Instructions:</h2>
             <h3>Please visit the link for cooking directions</h3>
-            {/* <span>{this.state.recipe.url}</span> */}
+            {this.state.recipe && 
+            <span>{this.state.recipe.url}</span>
+            }
         </div>
       
       </div>
